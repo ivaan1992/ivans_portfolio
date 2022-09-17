2a75e2fe35fceb96de019ce87de0eab8fdb84658
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-09-17 15:04:49 -0500

    Finish the home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import About from './pages/about/about';
 import Resume from './pages/resume/resume';
 import Skills from './pages/skills/skills';
 import Portfolio from './pages/portfolio/portfolio';
 import Contact from './pages/contact/contact';
-import './App.css';
+import './App.scss';
+
+
+import "primereact/resources/themes/luna-blue/theme.css";           //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+ 
 
 const App = () => (
   <div className="App">
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './home.scss';
+import HomeButton from '../../components/home-button/homeButton';
 
 const home = () => (
   <div className="d-flex home-section">
     <div className="texts d-flex">
       <h1 className="home-title">HELLO!<br /> WELCOME TO MY WORLD</h1>
-      <p className="home-bio">I hope you like the world that I have created.<br /> Thank you for watching it!!<br /> Have a great day.</p>
+      <p className="home-bio">I hope you like the world that I've created.<br /> Thanks for watching it!!<br /> Have a great day.</p>
+      <HomeButton />
     </div>
   </div>
 );
