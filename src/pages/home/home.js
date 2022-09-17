a8f4a31fc4e5806bdcb3cfc3ed7b1f15109b20b0
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-09-17 00:41:43 -0500

    Finish the setup

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './home.scss';
 
 const home = () => (
-  <div>
-    <h1>Home section</h1>
+  <div className="d-flex home-section">
+    <div className="texts d-flex">
+      <h1 className="home-title">HELLO!<br /> WELCOME TO MY WORLD</h1>
+      <p className="home-bio">I hope you like the world that I have created.<br /> Thank you for watching it!!<br /> Have a great day.</p>
+    </div>
   </div>
 );
 
