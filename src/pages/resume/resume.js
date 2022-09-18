935cb5bafb8c99d0da75ef35bbd1598572621011
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-09-18 14:23:39 -0500

    Finish the about me section

--- src/pages/resume/resume.js
+++ src/pages/resume/resume.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Sidebar from '../../components/sidebar/sidebar';
+import './resume.scss';
 
 const resume = () => (
-  <div className=''>
-    <div>
-    </div>
+  <div className='d-flex'>
     <div className="sidebar">
       <Sidebar />
+    </div>
+    <div className="resume d-flex">
+        <h1>Hello again!!</h1>
+        <p>In this section you can download my resume.
+        </p>
     </div>
   </div>
 );
